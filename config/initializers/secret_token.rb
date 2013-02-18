bc06171be6ba7e69af21171d1d528707f7a8d6af
Nana Kay <christian.osei-bonsu@meltwater.org>
2013-02-18 19:31:31 +0000

    Initian Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimeraApp::Application.config.secret_token = 'c1b702b074a1b629c4459ca82b1a6c907e9329b1f79f51c173c4bc4d1bdd5321e85d757dc2144c67e5fed6163cecb097ce0f3f34b5fcaf9c055b1d580cb69be7'
